e67d38019f1d389199e56625d29b6f767ad2d867
Harenabs21 <hei.harena.6@gmail.com>
2025-02-20 06:30:55 +0300

    feat: basic app for testing maestro

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView, Button, Text, TextInput, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [taps, setTaps] = useState(0);
+  const [text, setText] = useState('')
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <SafeAreaView>
+      <Button
+        title="Add one"
+        variant="primary"
+        onPress={() => setTaps(taps + 1)}
+      />
+      <Button
+        title="Add ten"
+        testID="add_ten"
+        onPress={() => setTaps(taps + 10)}
+      />
+      <Text>Number of taps: {taps}</Text>
+      <TextInput
+        testID="text_input"
+        placeholder="Change me!"
+        onChangeText={setText}
+      />
+      <Text>You typed: {text}</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
