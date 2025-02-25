c0fd562933eb830b354e3d8ff7ce3a07b96d3d21
Harenabs21 <hei.harena.6@gmail.com>
2025-02-25 16:05:50 +0300

    integrate infinite pokemon scroll list

--- App.jsx
+++ App.jsx
@@ -4,11 +4,11 @@ import usePokemon from './hook/use-pokemon';
 import PokemonCard from './components/PokemonCard';
 
 const App = () => {
-  const { pokemon, loading } = usePokemon();
+  const { pokemon, loading, fetchMore } = usePokemon();
 
-  if (loading) {
+  if (loading && pokemon.length === 0) {
     return (
-      <View style={styles.container}>
+      <View style={styles.loadingContainer}>
         <Text>Loading...</Text>
       </View>
     );
@@ -22,8 +22,12 @@ const App = () => {
     <FlatList
       data={pokemon}
       renderItem={renderPokemonCard}
-      keyExtractor={(item) => item.name}
+      keyExtractor={(item) => item.id.toString()}
       contentContainerStyle={styles.container}
+      onEndReached={fetchMore}
+      onEndReachedThreshold={0.5}
+      ListFooterComponent={loading && <Text>Loading more...</Text>}
+      initialNumToRender={10}
     />
   );
 };
@@ -32,6 +36,11 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
   },
+  loadingContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default App;
--- hook/use-pokemon.jsx
+++ hook/use-pokemon.jsx
@@ -3,18 +3,37 @@ import { useState, useEffect } from 'react';
 const usePokemon = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true)
+  const [nextUrl, setNextUrl] = useState('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
+
+  const extractIdFromUrl = (url) => {
+    const segments = url.split("/");
+    return segments[segments.length - 2]; 
+  };
 
   const fetchData = async () => {
 
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0");
+        if(!nextUrl) return;
+        setLoading(true)
+        const response = await fetch(nextUrl);
         const data = await response.json();
         const details = await Promise.all(data.results.map(async (poke) => {
-        const detailResponse = await fetch(poke.url);
-        const detailData = await detailResponse.json();
-        return { ...poke, image: detailData.sprites.other.showdown.front_default };
-        }));
-        setPokemon(details);
+            const detailResponse = await fetch(poke.url);
+            const detailData = await detailResponse.json();
+            return { ...poke,
+                     id: extractIdFromUrl(poke.url),
+                     image: detailData.sprites.other.showdown.front_default 
+                };
+        }
+      )
+    );
+        setPokemon((prev) => {
+            const uniquePokemons = details.filter(
+              (newPoke) => !prev.some((existingPoke) => existingPoke.id === newPoke.id)
+            );
+            return [...prev, ...uniquePokemons];
+          })
+        setNextUrl(data.next)
         setLoading(false)
   } catch (error) {
         console.error(error);
@@ -27,7 +46,7 @@ useEffect(() => {
   }, 
   []);
 
-  return { pokemon, loading };
+  return { pokemon, loading, fetchMore: fetchData };
 
 }
 
