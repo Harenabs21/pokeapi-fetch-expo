94a95052cc1168f8e209abbd9eda9d409c3f0fdd
Harenabs21 <hei.harena.6@gmail.com>
2025-02-25 15:05:53 +0300

    wrap fetchData with try catch

--- hook/use-pokemon.jsx
+++ hook/use-pokemon.jsx
@@ -4,23 +4,31 @@ const usePokemon = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0");
-      const data = await response.json();
-      const details = await Promise.all(data.results.map(async (poke) => {
+  const fetchData = async () => {
+
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0");
+        const data = await response.json();
+        const details = await Promise.all(data.results.map(async (poke) => {
         const detailResponse = await fetch(poke.url);
         const detailData = await detailResponse.json();
         return { ...poke, image: detailData.sprites.other.showdown.front_default };
-      }));
-      setPokemon(details);
-      setLoading(false)
-    };
+        }));
+        setPokemon(details);
+        setLoading(false)
+  } catch (error) {
+        console.error(error);
+        setLoading(false)
+    }
 
+}
+useEffect(() => {
     fetchData();
-  }, []);
+  }, 
+  []);
 
   return { pokemon, loading };
+
 }
 
 export default usePokemon;
