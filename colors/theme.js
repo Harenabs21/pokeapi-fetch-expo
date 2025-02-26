77aefadf99e1b23772591b8af1ee4a526a8a8cdc
Harenabs21 <hei.harena.6@gmail.com>
2025-02-26 12:09:16 +0300

    define the colors for theme

---
+++ colors/theme.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+    background: '#F5F5F5',
+    text: '#21130d',
+    borderColor: '#ddd'
+}
+
+
+export const darkTheme = {
+    background: '#082032',
+    text: '#F5F5F5',
+    borderColor: '#1ABC9C'
+}
