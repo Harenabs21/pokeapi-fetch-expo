c453ede2b8968ac1dd96d3cb141477fa13e19a45
Harenabs21 <hei.harena.6@gmail.com>
2025-02-27 17:24:19 +0300

    use object destructuratiion in theme

--- components/pokemon-list/PokemonCard.jsx
+++ components/pokemon-list/PokemonCard.jsx
@@ -4,14 +4,14 @@ import { ThemeContext } from '../../context/theme/ThemeContext'
 import tw from 'twrnc'
 
 const PokemonCard = React.memo(({ pokemon }) => {
-  const theme = useContext(ThemeContext)
+  const { background, text, borderColor } = useContext(ThemeContext);
 
   return (
-    <TouchableOpacity style={[tw`flex-row items-center p-2 border border-gray-300 rounded-md mb-2 `, {backgroundColor: theme.background}, {borderColor: theme.borderColor}]}>
+    <TouchableOpacity style={[tw`flex-row items-center p-2 border border-gray-300 rounded-md mb-2 `, {backgroundColor: background}, {borderColor: borderColor}]}>
       <View style={tw`w-18 h-18 mr-2`}>
         <Image source={{uri: pokemon.image}} style={[tw`w-full h-full rounded-md`, {resizeMode: 'contain'}]}/>
       </View>
-      <Text style={[tw`text-lg font-bold` , {color: theme.text}]}>{pokemon.name}</Text>
+      <Text style={[tw`text-lg font-bold` , {color: text}]}>{pokemon.name}</Text>
     </TouchableOpacity>
   )
 })
