b81e7fda19b9164707a0a955a97d1c64ad78417b
Harenabs21 <hei.harena.6@gmail.com>
2025-02-27 21:55:55 +0300

    add isGridView props and change styling

--- components/pokemon-list/PokemonCard.jsx
+++ components/pokemon-list/PokemonCard.jsx
@@ -3,15 +3,15 @@ import React, { useContext } from 'react'
 import { ThemeContext } from '../../context/theme/ThemeContext'
 import tw from 'twrnc'
 
-const PokemonCard = React.memo(({ pokemon }) => {
+const PokemonCard = React.memo(({ pokemon, isGridView }) => {
   const { background, text, borderColor } = useContext(ThemeContext);
 
   return (
-    <TouchableOpacity style={[tw`flex-row items-center p-2 border border-gray-300 rounded-md mb-2 `, {backgroundColor: background}, {borderColor: borderColor}]}>
-      <View style={tw`w-18 h-18 mr-2`}>
-        <Image source={{uri: pokemon.image}} style={[tw`w-full h-full rounded-md`, {resizeMode: 'contain'}]}/>
+    <TouchableOpacity style={[tw`${ isGridView ? 'flex-col items-center p-2 border rounded-md mb-4 w-40' : 'flex-row items-center p-2 border rounded-md mb-2'}`, {backgroundColor: background}, {borderColor: borderColor}]}>
+      <View style={tw`${isGridView ? 'mb-2 w-full h-32' : 'w-18 h-18 mr-2'}`}>
+        <Image source={{uri: pokemon.image}} style={[tw`w-full h-full rounded-lg`, {resizeMode: 'contain'}]}/>
       </View>
-      <Text style={[tw`text-lg font-bold` , {color: text}]}>{pokemon.name}</Text>
+      <Text style={[tw`${isGridView ? 'text-center text-lg font-bold' : 'text-lg font-bold'}` , {color: text}]}>{pokemon.name}</Text>
     </TouchableOpacity>
   )
 })
