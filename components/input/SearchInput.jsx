515ebabd000189abb4515dea4cbaedd5f7dad8fd
Harenabs21 <hei.harena.6@gmail.com>
2025-02-27 17:23:12 +0300

    create search input

---
+++ components/input/SearchInput.jsx
@@ -0,0 +1,23 @@
+import { View, TextInput } from 'react-native'
+import React, {useContext} from 'react'
+import tw from 'twrnc'
+import { FontAwesome } from '@expo/vector-icons'
+import { ThemeContext } from '../../context/theme/ThemeContext'
+const SearchInput = ({value, onChangeText}) => {
+
+  const { background, text, borderColor } = useContext(ThemeContext);
+
+  return (
+    <View style={[tw`flex-row items-center border rounded-md p-2 mb-4`, {backgroundColor: background, borderColor: borderColor}]}>
+      <FontAwesome name="search" size={20} color={text} style={tw`mr-2`} />  
+      <TextInput
+        style={tw`flex-1`}
+        placeholder="Search Pokémon..."
+        value={value}
+        onChangeText={onChangeText}
+        />
+    </View>
+  )
+}
+
+export default SearchInput
