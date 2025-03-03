3458356951788f2b34dcbb7d511c2cec05eb9536
Harenabs21 <hei.harena.6@gmail.com>
2025-03-03 17:00:34 +0300

    remove theme context and provider

--- App.jsx
+++ App.jsx
@@ -1,13 +1,10 @@
-import { ThemeProvider } from './providers';
 import PokemonList from './components/pokemon-list/PokemonList';
 import React from 'react';
 
 const App = () => {
   
   return (
-  <ThemeProvider>
     <PokemonList/>
-  </ThemeProvider>
   );
 };
 
