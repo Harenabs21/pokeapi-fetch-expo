c67c6edb44484c945b392f931306325e08f53489
Harenabs21 <hei.harena.6@gmail.com>
2025-03-03 18:18:01 +0300

    import useThemeListener

--- App.jsx
+++ App.jsx
@@ -1,7 +1,9 @@
 import PokemonList from './components/pokemon-list/PokemonList';
 import React from 'react';
+import { useThemeListener } from './hook/use-theme-listener';
 
 const App = () => {
+  useThemeListener()
   
   return (
     <PokemonList/>
