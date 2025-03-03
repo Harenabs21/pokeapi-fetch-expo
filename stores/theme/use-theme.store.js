e80523667174558b02e99698cfdc8d30eebf22ac
Harenabs21 <hei.harena.6@gmail.com>
2025-03-03 16:49:26 +0300

    create store for global theme using zustand

---
+++ stores/theme/use-theme.store.js
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+import { lightTheme, darkTheme } from "../../colors/theme";
+import { Appearance } from "react-native";
+
+const useThemeStore = create((set) => ({
+    theme: Appearance.getColorScheme() === "dark" ? darkTheme : lightTheme,
+    setTheme: (colorScheme) => set({ theme: colorScheme === "dark" ? darkTheme : lightTheme }),
+  }));
+
+Appearance.addChangeListener(({ colorScheme }) => {
+    useThemeStore.setTheme(colorScheme === "dark"? darkTheme : lightTheme);
+});
+
+export default useThemeStore;
