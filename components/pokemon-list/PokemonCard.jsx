b86223c42c425c2bde64ed6057989313fec2ce8c
Harenabs21 <hei.harena.6@gmail.com>
2025-03-07 15:52:34 +0300

    add background color for the card for each type of pokemon

--- components/pokemon-list/PokemonCard.jsx
+++ components/pokemon-list/PokemonCard.jsx
@@ -2,24 +2,32 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
 import useThemeStore from '../../stores/theme/use-theme.store';
+import getColorFromType from '../../colors/pokemon-color-type';
 
 const PokemonCard = React.memo(({ pokemon, isGridView }) => {
-  const { background, text, borderColor } = useThemeStore((state) => state.theme);
+  const { text, borderColor } = useThemeStore((state) => state.theme);
+  const { id, name, image, types } = pokemon;
+  const backgroundFromType = getColorFromType(types[0]);
 
   return (
     <TouchableOpacity
       style={[
         tw`${isGridView ? 'flex-col items-center p-2 border rounded-md mb-4 w-40 shadow-sm' : 'flex-row items-center p-2 border rounded-md mb-2'}`,
-        { backgroundColor: background },
+        { backgroundColor: backgroundFromType },
         { borderColor: borderColor },
       ]}
     >
       <View style={tw`${isGridView ? 'mb-2 w-full h-32' : 'w-18 h-18 mr-2'}`}>
-        <Image source={{ uri: pokemon.image }} style={[tw`w-full h-full rounded-lg`, { resizeMode: 'contain' }]} />
+        <Image source={{ uri: image }} style={[tw`w-full h-full rounded-lg`, { resizeMode: 'contain' }]} />
       </View>
-      <Text style={[tw`${isGridView ? 'text-center text-lg font-bold' : 'text-lg font-bold'}`, { color: text }]}>
-        {pokemon.name}
-      </Text>
+      <View>
+        <Text style={[tw`${isGridView ? 'text-center text-lg font-thin' : 'text-lg font-thin'}`, { color: text }]}>
+          {String(id).padStart(3, '0')}
+        </Text>
+        <Text style={[tw`${isGridView ? 'text-center text-lg font-bold' : 'text-lg font-bold'}`, { color: text }]}>
+          {name}
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 });
